9a7ac23cf30b9139ee2de2ceecd3617330174b55
nageswarao128 <73346292+nageswarao128@users.noreply.github.com>
2023-07-17 14:28:53 +0530

    Update main.tf

--- Tf files/main.tf
+++ Tf files/main.tf
@@ -10,60 +10,30 @@ terraform {
 provider "azurerm" {
   features {}
 }
-#name
-resource "random_integer" "ri" {
-  min = 10000
-  max = 99999
-}
 
-resource "azurerm_resource_group" "rg" {
-  name     = "myResourceGroup-${random_integer.ri.result}"
-  location = "eastus"
-}
 
 resource "azurerm_app_service_plan" "appserviceplan" {
-  name                = "webapp-asp-${random_integer.ri.result}"
+  name                = "webapp-asp"
   location            = "eastus"
-  resource_group_name = azurerm_resource_group.rg.name
+  resource_group_name = "NageswarRG"
   sku {
     tier = "Free"
     size = "F1"
   }
 }
 
-resource "azurerm_app_service" "webapp" {
-  name                = "webapp-${random_integer.ri.result}"
-  location            = "eastus"
-  resource_group_name = azurerm_resource_group.rg.name
-  app_service_plan_id = azurerm_app_service_plan.appserviceplan.id
-#   source_control {
-#     repo_url           = "https://github.com/myprofilr/Cera-new-code.git"
-#     branch             = "master"
-#     manual_integration = true
-#     use_mercurial      = false
-#   }
-}
+# resource "azurerm_app_service" "webapp" {
+#   name                = "webapp128"
+#   location            = "eastus"
+#   resource_group_name = "NageswarRG"
+#   app_service_plan_id = azurerm_app_service_plan.appserviceplan.id
+# }
 
-resource "azurerm_app_service" "webapp11"{
-    name  = "webapp12-${random_integer.ri.result}"
-    location = "eastus"
-    resource_group_name = azurerm_resource_group.rg.name
-    app_service_plan_id = azurerm_app_service_plan.appserviceplan.id
-}
+
 
 resource "azurerm_app_service" "webapp12"{
-    name  = "webapp13-${random_integer.ri.result}"
+    name  = "webapp13"
     location = "eastus"
-    resource_group_name = azurerm_resource_group.rg.name
+    resource_group_name = "NageswarRG"
     app_service_plan_id = azurerm_app_service_plan.appserviceplan.id
-}
-
-
-resource "azurerm_sql_server" "myresource" {
-   name = "sqlserver-128"
-   resource_group_name = azurerm_resource_group.rg.name
-   location = "eastus"
-   version = "12.0"
-   administrator_login = "mradministrator"
-   administrator_login_password = "thisIspnr@128"
 }
