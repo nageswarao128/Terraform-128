834222b39e233c1a99034c137e0a572849cc3cee
nageswarao128 <73346292+nageswarao128@users.noreply.github.com>
2023-07-17 17:44:40 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -14,7 +14,6 @@ provider "azurerm" {
   tenant_id              = "73d3d213-f87a-4465-9a7e-67bd625fdf9c"
   client_id                = "b57b2822-9a42-44c6-b239-d37ff4a8e782"
   client_secret      = "Q3w8Q~kFXyWJDQFzq6CnoYJNfrTUBE5RmnZR_ai_"
-  features {}
 }
 
 
